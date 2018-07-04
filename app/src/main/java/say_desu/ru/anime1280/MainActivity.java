be47216d5337aa857c31cdf6f8df5d260ea791cd
Dmitry Kravchenko <dmitriyk813@gmail.com>
2018-07-04 21:12:28 +0300

    Work started
    Main window layout created

---
+++ app/src/main/java/say_desu/ru/anime1280/MainActivity.java
@@ -0,0 +1,24 @@
+package say_desu.ru.anime1280;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
